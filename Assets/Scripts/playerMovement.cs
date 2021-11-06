50a24d50aa379fbea7bae72606e73edec6609960
DESKTOP-SD3Q2KO\Admin <divinemonster123@gmail.com>
2021-11-06 23:25:44 +0100

    base wasd movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private void Start() {
+
+    }
+
+    public float _playerSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update() {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * _playerSpeed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * _playerSpeed * horizontalInput);
+
+    }
+}
