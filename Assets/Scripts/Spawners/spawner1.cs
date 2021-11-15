03fd1b9110dbf00aff0e8d6c1928d99df56e0fd9
DESKTOP-SD3Q2KO\Admin <divinemonster123@gmail.com>
2021-11-15 22:27:10 +0100

    added spawners, move waves, reprogramed spawner scripts

---
+++ Assets/Scripts/Spawners/spawner1.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner1 : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float waveStartTime = 10f;
+    [SerializeField] private float gameTime = 0f;
+    [SerializeField] private float enemySpawnDelay = 0.75f;
+
+    private float timeElapsed = 0f;
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+        timeElapsed += Time.deltaTime;
+
+        if (gameTime > waveStartTime)
+        {
+            if (timeElapsed >= enemySpawnDelay)
+            {
+                GameObject newEnemy = Instantiate(enemyPrefab, new Vector3(Random.Range(-5f, 5f), Random.Range(1f, 1f), 1), Quaternion.identity);
+                timeElapsed = 0f;
+                OutputTime();
+            }
+        }
+    }
+
+    void OutputTime()
+    {
+        Debug.Log(Time.time);
+    }
+
+
+}
--- Assets/Scripts/bulletMovement.cs
+++ Assets/Scripts/bulletMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class bulletMovement : MonoBehaviour
 {
-    public float bulletSpeed = 35f;
+    public float bulletSpeed = 40f;
     public int bulletDamage = 1;
 
 
