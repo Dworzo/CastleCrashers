e4b7a0c7859bde02aedd2f016458cb2dff1aec95
JanPalmer <janrobertpalmer@gmail.com>
2021-12-02 20:29:18 +0100

    Dodanie NavMeshy
    - Dodanie biblioteki NavMeshComponents do Assets
    - Utworzenie folderu Enemies na (jak na razie) prefaby związane z przeciwnikami
    - Dodanie NavMesha do sceny Krzysztof_1 (Trzeba bake'ować navmesha dla każdego przeciwnika typu przeciwnika osobno)
    - Zastąpienie obecnego pathfindingu dla przeciwników tym z navmeshami (NavMeshAgent):
    Czerwony nadal idzie prosto na nas
    Złoty zatrzymuje się w dystansie Attack Range (stoi w miejscu jak próbujemy podchodzić)
    - Dodanie navmeshowego collidera/cokolwiek to jest do Playera, żeby Czerwony zatrzymywał się obok Playera a nie wchodził pod niego

--- Assets/Scripts/enemy Scripts/enemyMove.cs
+++ Assets/Scripts/enemy Scripts/enemyMove.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyMove : MonoBehaviour
 {
     private float rotationSpeed = 10.0f;
     private Transform player;
+    public NavMeshAgent agent;
 
     // Update is called once per frame
     void Update()
     {
-        //running towards player
+        //Navigating towards player
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        transform.position += transform.forward * 5f * Time.deltaTime;
+        agent.SetDestination(player.transform.position);
+
+        //running towards player
+        //Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //transform.position += transform.forward * 5f * Time.deltaTime;
     }
 }
