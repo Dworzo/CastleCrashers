0bb2decc9ff482ce107a7321d483a5738a4d60e1
Bartosz Han <bartoszhan4@gmail.com>
2021-12-30 20:38:30 +0100

    Added previev of shoot beams for 'snipers'.

--- Assets/Scripts/enemy Scripts/rangeEnemy.cs
+++ Assets/Scripts/enemy Scripts/rangeEnemy.cs
@@ -20,12 +20,16 @@ public class rangeEnemy : MonoBehaviour
 
     private float canAttack;
     private float timeBtwShots;
+     // private float aimingTime;
     private float rotationSpeed = 10f;
     private float distance;
     private Vector3 retreatPosition;
     private Quaternion targetRotation;
 
     public NavMeshAgent agent;
+
+    public GameObject Line;
+    public LineRenderer shootBeam;
 
     // Start is called before the first frame update
     void Start()
@@ -34,14 +38,20 @@ public class rangeEnemy : MonoBehaviour
         currentHealth = maxHealth;
         timeBtwShots = startTimeBtwShots;
         agent.stoppingDistance = attackRange;
+
+        shootBeam = GetComponentInChildren<LineRenderer>();
+        shootBeam.useWorldSpace = true;
+        shootBeam.startColor = Color.red;
+        shootBeam.endColor = Color.red;
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate()
+    {   
         canAttack += Time.deltaTime;
-        distance = Vector3.Distance(transform.position, player.position);
-
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        showShootBeam(gameObject.transform.position, transform.TransformDirection(Vector3.forward));
         //running towards player
         //transform.position += transform.forward * 5f * Time.deltaTime;
 
@@ -109,7 +119,26 @@ public class rangeEnemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    
+    public void showShootBeam(Vector3 position, Vector3 direction)
+    {
+        // Create visisble raycat beam, which shows where the enemy is aiming.
+
+        RaycastHit hit;
+        int layerMask = 1 << 7;
+        layerMask = ~layerMask;
+
+        if (Physics.Raycast(position, direction, out hit, Mathf.Infinity, layerMask))
+        {
+            print(hit.collider.gameObject.name);
+            print("pos:" + position);
+            print("hit:" + hit.transform.position);
+            print(hit.point);
+            shootBeam.SetPosition(1, position);
+            shootBeam.SetPosition(0, new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z));
+            
+        }
+        Debug.DrawRay(position, direction * 100f, Color.cyan);
+    }
 
 
 }
